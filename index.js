fffdcded0cde1fce05add12edc13c964a67e35ff
Dev Kaliper <gxlthz@gmail.com>
2023-07-09 21:57:14 -0500

    VS-4 first node .

---
+++ index.js
@@ -0,0 +1,15 @@
+//  Require the necesary discord.js classes
+const { Client, Events, GatewayIntentBits } = require('discord.js');
+const { token } = require('./config.json');
+
+// Create a new client instance
+const client = new Client({ intents: [GatewayIntentBits.Guilds] });
+
+// When the client is ready, run this code (only once)
+client.once(Events.ClientReady, (c) => {
+	console.log(` Ready! Logged in as ${c.user.tag}!`);
+});
+
+
+// Login to Discord with your client's token
+client.login(token);
